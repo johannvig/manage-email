24ebb5d42404ecdfb42b8c25e88816f6cdbbb7d5
Vigouroux Johanne <102874093+johannvig@users.noreply.github.com>
2024-07-03 16:42:27 +0200

    Update send mail.py

--- send mail/send mail.py
+++ send mail/send mail.py
@@ -8,26 +8,24 @@ from time import sleep
 
 warnings.filterwarnings('ignore')
 
-
 # WARNING: Active IMAP and less secure app before on every email you want to use with this script
-# Note: The script can only be used with gmail, outlook and yahoo
+# Note: The script can only be used with Gmail, Outlook, and Yahoo
 
-def send_mail():
-
+def send_mail(row_number, file_path, webhook_url, discord_bot_name, discord_bot_icon):
     try:
-        with open('', 'r') as csv_file: #put the path of "send mail.csv" file that you can download in the project (ex of a path: "../email/send mail.csv")
+        with open(file_path, 'r') as csv_file:  # path of "send mail.csv" file
             csv_reader = reader(csv_file)
             list_of_rows = list(csv_reader)
 
-            row_number = s
             col_number = 1
             email_name = list_of_rows[row_number - 1][col_number - 1]
 
             # filter according to the type of email and uses the SMTP protocol associated with the type of email
-
             gmail = ["@gmail.com"]
             outlook = ["@outlook.com"]
             yahoo = ["@yahoo.com"]
+
+            smtpserver = None
 
             for email in outlook:
                 if email in email_name:
@@ -41,7 +39,9 @@ def send_mail():
                 if email in email_name:
                     smtpserver = smtplib.SMTP("smtp.gmail.com", 587)  # use gmail with port
 
-            # If you want to use this tool with another email box provider, you have to search the SMTP protocol of this email box provider and replace "smtp.gmail.com" by the new one
+            if smtpserver is None:
+                print(Fore.RED + "No suitable SMTP server found for the email: " + email_name)
+                return
 
             col_number = 2
             email_password = list_of_rows[row_number - 1][col_number - 1]
@@ -56,7 +56,6 @@ def send_mail():
             mail_message = list_of_rows[row_number - 1][col_number - 1]
 
             # Create SMTP session for to send the mail
-
             smtpserver.ehlo()  # identify the computer system
             smtpserver.starttls()  # enable security
 
@@ -65,23 +64,22 @@ def send_mail():
 
             # Send mail
             header = "To:" + email_receiver + "\n" + 'From: ' + email_name + '\n' + 'Subject:' + mail_subject + '\n'
-            print(Fore.WHITE+header)
+            print(Fore.WHITE + header)
             msg = header + '\n ' + mail_message + ' \n\n'
             smtpserver.sendmail(email_name, email_receiver, msg)
 
             smtpserver.close()
-            print(Fore.GREEN+'Successfully send')
+            print(Fore.GREEN + 'Successfully sent')
 
             # Track all mail that you sent with a discord webhook
-
-            webhook = DiscordWebhook(url="")  # put your discord webhook url
-            embed = DiscordEmbed(title="task sucess", color=3066993)
+            webhook = DiscordWebhook(url=webhook_url)  # put your discord webhook url
+            embed = DiscordEmbed(title="Task Success", color=3066993)
             embed.set_author(
-                name="",  # put the name of your discord bot
-                icon_url="",  # put the profil image of your discord bot
+                name=discord_bot_name,  # put the name of your discord bot
+                icon_url=discord_bot_icon,  # put the profile image of your discord bot
             )
-            embed.add_embed_field(name='email', value=email_name, inline=False)
-            embed.add_embed_field(name='Line of the csv', value=s, inline=False)
+            embed.add_embed_field(name='Email', value=email_name, inline=False)
+            embed.add_embed_field(name='Line of the CSV', value=row_number, inline=False)
             embed.add_embed_field(name='Tool', value="Send email", inline=False)
             webhook.add_embed(embed)
             embed.set_timestamp()
@@ -90,34 +88,36 @@ def send_mail():
             g = randint(300, 600)
             v = g / 100
             sleep(v)
-            print("sleep during " + str(v) + "seconde")
+            print("Sleeping for " + str(v) + " seconds")
     except Exception as e:
         print(Fore.RED + "Ohoh! Something went wrong!")
         print(e)
 
 
+if __name__ == "__main__":
+    file_path = ''  # put the path of "send mail.csv" file
+    webhook_url = ''  # put your discord webhook url
+    discord_bot_name = ''  # put the name of your discord bot
+    discord_bot_icon = ''  # put the profile image url of your discord bot
 
-k = "yes"
-s = 2
-while True:
-    with open('', 'r') as csv_file: #put the path of "send mail.csv" file that you can download in the project  
-        csv_reader = reader(csv_file)
-        list_of_rows = list(csv_reader)
-        # (list_of_rows)
+    k = "yes"
+    s = 2
+    while True:
+        with open(file_path, 'r') as csv_file:  # path of "send mail.csv" file
+            csv_reader = reader(csv_file)
+            list_of_rows = list(csv_reader)
 
-        try:
-            row_number = s
-            col_number = 1
-            value = list_of_rows[row_number - 1][col_number - 1]
-        except:
-            k = "no"
+            try:
+                row_number = s
+                col_number = 1
+                value = list_of_rows[row_number - 1][col_number - 1]
+            except IndexError:
+                k = "no"
 
-        if k == "no":
-            print(Fore.GREEN + "program is over")
-            print(Fore.GREEN + "Job done. Enjoy your day!")
-            break
-
-
-        else:
-            send_mail()
-            s += 1
+            if k == "no":
+                print(Fore.GREEN + "Program is over")
+                print(Fore.GREEN + "Job done. Enjoy your day!")
+                break
+            else:
+                send_mail(row_number, file_path, webhook_url, discord_bot_name, discord_bot_icon)
+                s += 1
