c09dbb863edb8a0bbbfc62126adef11db3ee90f0
Vigouroux Johanne <102874093+johannvig@users.noreply.github.com>
2024-07-03 16:24:37 +0200

    Create liaison.py

---
+++ liaison.py
@@ -0,0 +1,6 @@
+dict = {}
+dict[1] = lambda : exec(open('delete mail/delete mail.py').read())
+dict[2] = lambda : exec(open('read mail/read mail.py').read())
+dict[3] = lambda : exec(open('scrape mail/scrape mail.py').read())
+dict[4] = lambda : exec(open('send mail/send mail.py').read())
+dict[5] = lambda : exec(open('verify email/verify email.py').read())
